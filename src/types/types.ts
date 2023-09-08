1934ebc5488b3708371175b0d25ba5ad1e6022f7
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-08 22:26:16 +0300

    Types update

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,10 @@
-export interface ICategory {
-  name: string;
-  colorScheme: string;
+export interface IQuiz {
+  id: number;
+  category: string;
+  topic: string;
+  question: string;
+  answer: string;
+  fakeAnswer1: string;
+  fakeAnswer2: string;
+  fakeAnswer3: string;
 }
