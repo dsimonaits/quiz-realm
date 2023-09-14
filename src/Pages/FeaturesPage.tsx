02c1c4c2b1a4bd86197dd8160023e96ffd564a8d
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-14 14:13:29 +0300

    Delete FeaturesPage.tsx

--- src/Pages/FeaturesPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Features = () => {
-  return (
-    <div style={{ textAlign: "center", padding: "20px" }}>Features page</div>
-  );
-};
-
-export default Features;
