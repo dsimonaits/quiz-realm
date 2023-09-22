b2a76d717d8772ba9247ddde7570aa9489e15a47
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-22 16:30:51 +0300

    Text compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ViteCompressionPlugin from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/quiz-realm",
-  plugins: [react()],
+  plugins: [react(), ViteCompressionPlugin()],
   build: {
     outDir: "build",
   },
