2d43f5b866b902788a92f1396b66652b4cadc2bc
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-22 12:08:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/quiz-realm",
-  plugins: [react()],
-  build: {
-    outDir: "build",
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/quiz-realm/";
+  }
+
+  return config;
 });
