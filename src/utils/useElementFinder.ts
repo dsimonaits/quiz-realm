7ef6daa675cc6e77b846785cd9a0aafa55ae51fe
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-23 01:01:39 +0300

    created useHook for searching document elements by data atribute

---
+++ src/utils/useElementFinder.ts
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef, RefObject } from "react";
+
+function useElementFinder({
+  dataSetName,
+  dependencies: dependencies,
+}: {
+  dataSetName: string;
+  dependencies: string[];
+}): RefObject<HTMLElement[] | null> {
+  const foundElementRef = useRef<HTMLElement[] | null>(null);
+
+  useEffect(() => {
+    const htmlElements = document.querySelectorAll<HTMLElement>(
+      `div[${dataSetName}]`
+    );
+
+    foundElementRef.current = Array.from(htmlElements);
+  }, [dependencies]);
+
+  return foundElementRef;
+}
+
+export default useElementFinder;
