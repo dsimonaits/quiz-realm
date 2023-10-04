4eececffb2322fafcfc09babc22ba39c7638f74c
Deniss Simonaits <dsimonaits@gmail.com>
2023-10-05 01:10:19 +0300

    Updates

--- assets/chunk-3ASUQ6PA-d25d3849.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./chunk-ZHMYA64R-2c0f4a4c.js";import{f as o,j as s}from"./index-d028ff7c.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"row",ref:r}));e.displayName="HStack";export{e as H};
--- assets/chunk-7OLJDQMT-5adb5b40.js
+++
@@ -1,1 +0,0 @@
-import{f as i,a as t,o as r,j as c,b as o,d as m}from"./index-d028ff7c.js";var d=i(function(a,s){const e=t("Heading",a),{className:f,...n}=r(a);return c.jsx(o.h2,{ref:s,className:m("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
