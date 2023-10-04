e39a64f905a65d283756f5317ad95582d829780b
Deniss Simonaits <dsimonaits@gmail.com>
2023-10-05 00:21:42 +0300

    Padding Y update

--- src/components/Layouts/Container/Container.tsx
+++ src/components/Layouts/Container/Container.tsx
@@ -3,15 +3,17 @@ import React, { FC } from "react";
 
 interface ICon {
   children: React.ReactNode;
+  style?: object;
 }
 
-const MainContainer: FC<ICon> = ({ children }) => {
+const MainContainer: FC<ICon> = ({ children, style }) => {
   return (
     <Container
       position="relative"
       mx="auto"
       px={["20px", "20px", "40px"]}
       maxWidth="1200px"
+      {...style}
     >
       {children}
     </Container>
