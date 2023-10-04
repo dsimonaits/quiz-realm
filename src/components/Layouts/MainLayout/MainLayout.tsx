6ba95d4cbc07d26346c67ab46ce28bf2ecaf14ee
Deniss Simonaits <dsimonaits@gmail.com>
2023-10-05 00:38:22 +0300

    Update MainLayout.tsx

--- src/components/Layouts/MainLayout/MainLayout.tsx
+++ src/components/Layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
-import { Divider } from "@chakra-ui/react";
 
 const MainLayout = () => {
   return (
@@ -9,7 +8,6 @@ const MainLayout = () => {
       <main>
         <Outlet />
       </main>
-      <Divider mt="10px" />
     </>
   );
 };
