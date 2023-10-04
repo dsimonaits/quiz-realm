ed29649e44c9263249fd5f4b5cee25e09344d7f5
Deniss Simonaits <dsimonaits@gmail.com>
2023-10-05 00:22:17 +0300

    Markup update

--- src/components/UI/QuizResult/QuizResult.tsx
+++ src/components/UI/QuizResult/QuizResult.tsx
@@ -1,6 +1,7 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 import { FC } from "react";
 import CustomButton from "../Button/Button";
+import MainContainer from "../../Layouts/Container/Container";
 
 interface IResult {
   good: number;
@@ -14,14 +15,21 @@ interface IQuizResult {
 
 const QuizResult: FC<IQuizResult> = ({ result, btnHandle }) => {
   return (
-    <>
-      <p>
-        Good: {result.good} <br /> Fault: {result.fault}
-      </p>
-      <HStack justify="center" gap="20px">
-        <CustomButton onClickHandler={btnHandle}>Complete</CustomButton>
-      </HStack>
-    </>
+    <MainContainer
+      style={{
+        minHeight: "400px",
+        p: "50px",
+      }}
+    >
+      <VStack align="center">
+        <p>
+          Good: {result.good} <br /> Fault: {result.fault}
+        </p>
+        <HStack justify="center" gap="20px">
+          <CustomButton onClickHandler={btnHandle}>Complete</CustomButton>
+        </HStack>
+      </VStack>
+    </MainContainer>
   );
 };
 
