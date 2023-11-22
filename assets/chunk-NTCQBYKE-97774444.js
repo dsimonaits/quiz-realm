3665e0d6dbebdea72011bc64d9f66c7b5a51c68f
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-22 13:24:47 +0200

    Updates

--- assets/chunk-NTCQBYKE-97774444.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./Container-812dd285.js";import{f as o,j as s}from"./index-56c9332a.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
