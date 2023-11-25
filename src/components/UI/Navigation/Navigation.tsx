030160fce25243dbfc348a8d2938d4f25c44fe32
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-25 22:01:57 +0200

    Update Navigation.tsx

--- src/components/UI/Navigation/Navigation.tsx
+++ src/components/UI/Navigation/Navigation.tsx
@@ -1,24 +1,42 @@
 import { FC } from "react";
 import { NavLink as ReactRouterLink } from "react-router-dom";
-import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ChakraLink, MenuItem } from "@chakra-ui/react";
 import { ILink } from "../../../types/types";
 
 interface NavProps {
   links: ILink[];
+  isMenu?: boolean;
 }
 
-const Navigation: FC<NavProps> = ({ links }) => {
-  return links.map((link: ILink) => (
-    <ChakraLink
-      as={ReactRouterLink}
-      to={link.path}
-      key={link.path}
-      _activeLink={{ color: "accent", fontWeight: "bold" }}
-      _hover={{ color: "accent" }}
-    >
-      {link.name}
-    </ChakraLink>
-  ));
+const Navigation: FC<NavProps> = ({ links, isMenu = false }) => {
+  return (
+    <>
+      {!isMenu
+        ? links.map((link: ILink) => (
+            <ChakraLink
+              as={ReactRouterLink}
+              to={link.path}
+              key={link.name}
+              _activeLink={{ color: "accent", fontWeight: "bold" }}
+              _hover={{ color: "accent" }}
+            >
+              {link.name}
+            </ChakraLink>
+          ))
+        : links.map((link: ILink) => (
+            <MenuItem key={link.name}>
+              <ChakraLink
+                as={ReactRouterLink}
+                to={link.path}
+                _activeLink={{ color: "accent", fontWeight: "bold" }}
+                _hover={{ color: "accent" }}
+              >
+                {link.name}
+              </ChakraLink>
+            </MenuItem>
+          ))}
+    </>
+  );
 };
 
 export default Navigation;
