1c34408b81731b35945642c09f804545322c7578
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-25 22:02:04 +0200

    Update AuthPage.tsx

--- src/Pages/AuthPage.tsx
+++ src/Pages/AuthPage.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import Section from "../components/Layouts/Section/Section";
 import LoginForm from "../components/form/LoginForm/LoginForm";
-import { Container, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import RegistrationForm from "../components/form/RegForm/RegForm";
+import welcomeHat from "../images/Hands Graduate.svg";
 
 const AuthPage = () => {
   const [formType, setFormType] = useState<"Login" | "Register">("Login");
@@ -21,6 +30,22 @@ const AuthPage = () => {
         alignItems="center"
         mx="auto"
       >
+        <HStack position="relative" mb={["20px", "20px", "20px"]}>
+          <Heading as="h1" fontSize={[40, 40, 40]}>
+            Quiz
+            <Box as="span" color="accent">
+              Realm
+            </Box>
+          </Heading>
+          <Box
+            position="absolute"
+            top={["-10px", "-10px", "-10px"]}
+            left={["-20px", "-20px", "-20px"]}
+          >
+            <Image src={welcomeHat} height="40px" />
+          </Box>
+        </HStack>
+
         <>
           {formType === "Login" ? <LoginForm /> : <RegistrationForm />}
           <Text mt={4}>
