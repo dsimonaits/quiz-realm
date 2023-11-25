44a5d6622f303513befcbd70e77090befe6581fb
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-25 22:01:51 +0200

    Create styles.ts

---
+++ src/components/form/RegForm/styles.ts
@@ -0,0 +1,20 @@
+export const InputStyles = {
+  p: "0",
+  height: "20px",
+  border: "none",
+  borderColor: "transparent !important",
+  focusBorderColor: "transparent",
+  errorBorderColor: "transparent",
+  color: "accent",
+};
+
+export const FormControlStyles = {
+  _notLast: { marginBottom: "-2px" },
+  p: "5px",
+  pl: "15px",
+  border: "2px solid",
+  borderColor: "background",
+  _focusWithin: {
+    shadow: "inset 10px 0px 0px 0px  var(--secondaryColor)",
+  },
+};
