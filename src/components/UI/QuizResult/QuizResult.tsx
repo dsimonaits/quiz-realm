614e62503dc7d01e19d2d198bb0885e8142b3c3c
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-28 00:09:16 +0200

    Update QuizResult.tsx

--- src/components/UI/QuizResult/QuizResult.tsx
+++ src/components/UI/QuizResult/QuizResult.tsx
@@ -1,7 +1,16 @@
-import { HStack, VStack } from "@chakra-ui/react";
+import {
+  HStack,
+  VStack,
+  Image,
+  Box,
+  Heading,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import CustomButton from "../Button/Button";
 import { IResult } from "../../../types/types";
+import ScoreImage from "../../../images/score.png";
 
 interface IQuizResult {
   result: IResult;
@@ -11,10 +20,41 @@ interface IQuizResult {
 const QuizResult: FC<IQuizResult> = ({ result, btnHandle }) => {
   return (
     <>
-      <VStack justify="center" minHeight="400px" p="50px">
-        <p>
-          Good: {result.good} <br /> Fault: {result.fault}
-        </p>
+      <VStack
+        justify="center"
+        mx="auto"
+        maxWidth={["300px", "400px", "500px"]}
+        p="20px"
+      >
+        <Box position="relative">
+          {" "}
+          <Image src={ScoreImage} />
+          <Flex
+            direction="column"
+            align="center"
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Heading as="h1" fontSize={[20, 30, 30]}>
+              You score
+            </Heading>
+            <Text color="secondary">
+              Good:{" "}
+              <Box as="span" color="#32CD32">
+                {result.good}
+              </Box>
+            </Text>
+            <Text color="secondary">
+              Fault:{" "}
+              <Box as="span" color="#CD5C5C">
+                {result.fault}
+              </Box>
+            </Text>
+          </Flex>
+        </Box>
+
         <HStack justify="center" mt="20px">
           <CustomButton onClickHandler={btnHandle}>Complete</CustomButton>
         </HStack>
