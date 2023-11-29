0625bc100fbb0dfeb153329c578712c75d3375b8
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-29 14:54:40 +0200

    Update HomePage.tsx

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,16 +1,21 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { FC } from "react";
 import Welcome from "../components/Layouts/Welcome/Welcome";
 import Section from "../components/Layouts/Section/Section";
 import MainContainer from "../components/Layouts/Container/Container";
 import Loader from "../components/UI/Loader/Loader";
 import { ScaleFade } from "@chakra-ui/react";
+import QuizStore from "../store/QuizStore";
 
 const StartLearning = lazy(
   () => import("../components/form/StartLearning/StartLearning")
 );
 
 const HomePage: FC = () => {
+  useEffect(() => {
+    QuizStore.getAllQuestions();
+  }, []);
+
   return (
     <ScaleFade
       in={true}
